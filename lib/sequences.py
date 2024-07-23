e52593186b77df6152788861feb7dc6794d6f5fa
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-23 18:59:39 -0500

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        result = []
+    elif length == 1:
+        result = [0]
+    elif length == 2:
+        result = [0, 1]
+    else:
+        result = [0, 1]
+        while len(result) < length:
+            next = result[len(result) - 1] + result[len(result) - 2]
+            result.append(next)
+    print(result)
